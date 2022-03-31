ac595777d1fe467843fa0bc7ad1a3673c9233758
Yu Zhang <60720097+YUZHANG-11@users.noreply.github.com>
2022-04-01 07:55:07 +1100

    Use design patterns to organize and avoid duplication of code

--- MyClient.java
+++ MyClient.java
@@ -2,30 +2,55 @@ import java.io.*;
 import java.net.*;
 
 public class MyClient {
-    public static void main(String[] args) {
+
+    private Socket socket;
+    private DataOutputStream out;
+    private BufferedReader in;
+
+    public MyClient() {
         try {
-            Socket socket = new Socket("127.0.0.1", 50000);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out.write(("HELO\n").getBytes());
-            out.flush();
-            String response = in.readLine();
-            System.out.println("HELO response= " + response);
-            String username = System.getProperty("user.name");
-            String command = "AUTH " + username + "\n";
-            out.write(command.getBytes());
-            out.flush();
+            socket = new Socket("127.0.0.1", 50000);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendMessage(String cmd) {
+        byte[] cmdBytes = (cmd + "\n").getBytes();
+        String response = "";
+        try {
+            out.write(cmdBytes);
             response = in.readLine();
-            System.out.println("AUTH response= " + response);
-            out.write(("REDY\n").getBytes());
-            response = in.readLine();
-            System.out.println("AUTH response= " + response);
-            out.write(("QUIT\n").getBytes());
             out.flush();
-            response = in.readLine();
-            System.out.println("QUIT: " + response);
+            System.out.println(cmd + " response = " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public void close() {
+        try {
             out.close();
             socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyClient client = new MyClient();
+            client.sendMessage("HELO");
+            client.sendMessage("AUTH " + System.getProperty("user.name"));
+            client.sendMessage("REDY");
+            client.sendMessage("GETS All");
+            client.sendMessage("OK");
+            client.sendMessage("QUIT");
+            client.close();
+
         } catch (Exception e) {
             System.out.println(e);
         }
