a871052115a68d767831bcb324994e6c925e7a73
Yu Zhang <60720097+YUZHANG-11@users.noreply.github.com>
2022-04-01 08:06:31 +1100

    Split the server node obtained by each "GETSAll" by the "\n" and " ", and realize their properties.

--- MyClient.java
+++ MyClient.java
@@ -1,15 +1,20 @@
 import java.io.*;
 import java.net.*;
+import java.nio.CharBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyClient {
 
     private Socket socket;
     private DataOutputStream out;
     private BufferedReader in;
+    private CharBuffer cb;
 
     public MyClient() {
         try {
             socket = new Socket("127.0.0.1", 50000);
+            cb = CharBuffer.allocate(1024);
             out = new DataOutputStream(socket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
@@ -22,9 +27,12 @@ public class MyClient {
         String response = "";
         try {
             out.write(cmdBytes);
-            response = in.readLine();
-            out.flush();
+            in.read(cb);
+            cb.flip();
+            response = cb.toString();
             System.out.println(cmd + " response = " + response);
+            cb.clear();
+            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,13 +48,37 @@ public class MyClient {
         }
     }
 
+    public List<ServerNode> getServerList(String servers) {
+        String[] lines = servers.split("\n");
+        List<ServerNode> serverNodeList = new ArrayList<>();
+        for (String line : lines) {
+            String[] elements = line.split(" ");
+            ServerNode serverNode = new ServerNode();
+
+            serverNode.setServerType(elements[0]);
+            serverNode.setServerID(Integer.parseInt(elements[1]));
+            serverNode.setState(elements[2]);
+            serverNode.setCurStartTime(Integer.parseInt(elements[3]));
+            serverNode.setCore(Integer.parseInt(elements[4]));
+            serverNode.setMemory(Integer.parseInt(elements[5]));
+            serverNode.setDisk(Integer.parseInt(elements[6]));
+            serverNode.setwJobs(Integer.parseInt(elements[7]));
+            serverNode.setrJobs(Integer.parseInt(elements[8]));
+
+            serverNodeList.add(serverNode);
+        }
+        return serverNodeList;
+    }
+
     public static void main(String[] args) {
         try {
             MyClient client = new MyClient();
+
             client.sendMessage("HELO");
             client.sendMessage("AUTH " + System.getProperty("user.name"));
             client.sendMessage("REDY");
             client.sendMessage("GETS All");
+            String servers = client.sendMessage("OK");
             client.sendMessage("OK");
             client.sendMessage("QUIT");
             client.close();
