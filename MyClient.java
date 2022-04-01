511744e00613d35d48a8490f843254eb1575d4a2
Yu Zhang <60720097+YUZHANG-11@users.noreply.github.com>
2022-04-02 07:53:28 +1100

    Finished job schedule use the biggest core node

--- MyClient.java
+++ MyClient.java
@@ -5,16 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyClient {
-
     private Socket socket;
     private DataOutputStream out;
     private BufferedReader in;
     private CharBuffer cb;
 
-    public MyClient() { // construct class fixed properties in Constructors
+    public MyClient() {
         try {
-            socket = new Socket("127.0.0.1", 50000); // port
-            cb = CharBuffer.allocate(1024);
+            socket = new Socket("127.0.0.1", 50000);
+            cb = CharBuffer.allocate(40960);
             out = new DataOutputStream(socket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
@@ -22,24 +21,24 @@ public class MyClient {
         }
     }
 
-    public String sendMessage(String cmd) { // create a method receive a command and return a response back
+    public String sendMessage(String cmd) {
         byte[] cmdBytes = (cmd + "\n").getBytes();
         String response = "";
         try {
             out.write(cmdBytes);
             in.read(cb);
             cb.flip();
-            response = cb.toString(); // get all server nodes with one line
-            System.out.println(cmd + " response = " + response);
+            response = cb.toString();
+            System.out.println(cmd + " response: " + response);
             cb.clear();
-            out.flush(); // after finish every write do flush
+            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return response;
     }
 
-    public void close() { // create a method to close the whole program process
+    public void close() {
         try {
             out.close();
             socket.close();
@@ -49,10 +48,10 @@ public class MyClient {
     }
 
     public List<ServerNode> getServerList(String servers) {
-        String[] lines = servers.split("\n"); // split server nodes by "\n"
+        String[] lines = servers.split("\n");
         List<ServerNode> serverNodeList = new ArrayList<>();
         for (String line : lines) {
-            String[] elements = line.split(" "); // split server nodes again by space
+            String[] elements = line.split(" ");
             ServerNode serverNode = new ServerNode();
 
             serverNode.setServerType(elements[0]);
@@ -63,36 +62,51 @@ public class MyClient {
             serverNode.setMemory(Integer.parseInt(elements[5]));
             serverNode.setDisk(Integer.parseInt(elements[6]));
             serverNode.setwJobs(Integer.parseInt(elements[7]));
-            serverNode.setrJobs(Integer.parseInt(elements[8])); // iterate over each server node and make them have attributes
+            serverNode.setrJobs(Integer.parseInt(elements[8]));
 
-            serverNodeList.add(serverNode); // add them into the Arraylist
+            serverNodeList.add(serverNode);
         }
         return serverNodeList;
     }
 
     public static void main(String[] args) {
-        try {
-            MyClient client = new MyClient();
 
-            client.sendMessage("HELO"); // establish connection
-            client.sendMessage("AUTH " + System.getProperty("user.name"));
-            client.sendMessage("REDY");
-            client.sendMessage("GETS All"); // achieve all server nodes
-            String servers = client.sendMessage("OK");
-            List<ServerNode> nodeList = client.getServerList(servers);
-            ServerNode biggestCoreNode = null;
-            for (ServerNode node : nodeList) {
-                if (biggestCoreNode == null || node.getCore() > biggestCoreNode.getCore()) { // compare the cores of each single node to find the largest
-                    biggestCoreNode = node;
-                }
+        MyClient client = new MyClient();
+
+        client.sendMessage("HELO");
+        client.sendMessage("AUTH " + System.getProperty("user.name"));
+        String jobStr = client.sendMessage("REDY");
+        client.sendMessage("GETS All");
+        String servers = client.sendMessage("OK");
+        List<ServerNode> nodeList = client.getServerList(servers);
+        ServerNode biggestCoreNode = null;
+        for (ServerNode node : nodeList) {
+            if (biggestCoreNode == null || node.getCore() > biggestCoreNode.getCore()) {
+                biggestCoreNode = node;
             }
-            System.out.println("\nBiggest Core Node: " + biggestCoreNode + "\n"); // print out the largest core node
-            client.sendMessage("OK");
-            client.sendMessage("QUIT"); // shutdown
-            client.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
         }
+        System.out.println("\nBiggest Core Node: " + biggestCoreNode + "\n");
+        client.sendMessage("OK");
+        while (!jobStr.equals("NONE\n")) {
+            if (jobStr.startsWith("JOBN")) {
+                Job job = client.getJobFromString(jobStr);
+                client.sendMessage("SCHD " + job.getJobID() + " " + biggestCoreNode.getServerType() + " " + biggestCoreNode.getServerID());
+            }
+            jobStr = client.sendMessage("REDY");
+        }
+        client.sendMessage("QUIT");
+        client.close();
+    }
+
+    private Job getJobFromString(String jobStr) {
+        Job job = new Job();
+        String[] elements = jobStr.split(" ");
+        job.setSubmitTime(Integer.parseInt(elements[1]));
+        job.setJobID(Integer.parseInt(elements[2]));
+        job.setEstRuntime(Integer.parseInt(elements[3]));
+        job.setCore(Integer.parseInt(elements[4]));
+        job.setMemory(Integer.parseInt(elements[5]));
+        job.setDisk(Integer.parseInt(elements[6].replace("\n", "")));
+        return job;
     }
 }
